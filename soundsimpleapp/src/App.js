f74eb1063826031e8c4b7161be3233f69bdf8bca
jamesbrignull <jamesbrignull@gmail.com>
2020-11-07 16:49:21 +0000

    state added and basic styling added

--- soundsimpleapp/src/App.js
+++ soundsimpleapp/src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 // Import styles
 import "./styles/app.scss";
+// Import Util
+import data from "./utils";
 // Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
 
 function App() {
+  // state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div>
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
