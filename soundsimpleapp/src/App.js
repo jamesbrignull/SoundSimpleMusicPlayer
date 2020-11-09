3f3c2b7519559b231524c3be20236a79a4e62d2d
jamesbrignull <jamesbrignull@gmail.com>
2020-11-09 20:03:33 +0000

    timetrack bar bug fixed

--- soundsimpleapp/src/App.js
+++ soundsimpleapp/src/App.js
@@ -20,6 +20,7 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
   const [libStatus, setLibStatus] = useState(false);
 
@@ -27,7 +28,16 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration });
+    // Calculating song percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation,
+    });
   };
 
   return (
